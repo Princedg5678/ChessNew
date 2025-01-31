af11f6ebd032faa5eac432ccff3088ad03e82431
Princedg5678 <gilesdevyn6@gmail.com>
2025-01-31 13:52:54 -0700

    Finished designing the gameBoard UI

--- client/src/main/java/ui/PrintBoard.java
+++ client/src/main/java/ui/PrintBoard.java
@@ -4,6 +4,9 @@ import chess.*;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 import static ui.EscapeSequences.*;
 
@@ -12,78 +15,85 @@ public class PrintBoard {
 
     public static void main(String[] args){
         printWhitePerspective(new ChessGame());
+        printBlackPerspective(new ChessGame());
     }
 
     public static void printWhitePerspective(ChessGame ourGame){
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
 
-        drawTopOrBottomRow(out);
-        drawBoardWhite(out, ourGame);
-        drawTopOrBottomRow(out);
+        drawTopOrBottomRow(out, true);
+        drawBoard(out, ourGame, true);
+        drawTopOrBottomRow(out, true);
+        out.print("\n");
 
     }
 
     public static void printBlackPerspective(ChessGame ourGame){
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
 
+        drawTopOrBottomRow(out, false);
+        drawBoard(out, ourGame, false);
+        drawTopOrBottomRow(out, false);
+        out.print("\n");
 
     }
 
-    private static void drawBoardWhite(PrintStream out, ChessGame game){
-
-        out.print("\n");
+    private static void drawBoard(PrintStream out, ChessGame game, boolean whitePerspective){
 
         ChessBoard theBoard = game.getBoard();
 
-        for (int i = 8; i > 0; i--){
-            out.print(SET_BG_COLOR_LIGHT_GREY);
-            out.print(" ");
-            out.print(SET_TEXT_COLOR_WHITE);
-            out.print(i);
-            out.print(" ");
-            for (int j = 8; j > 0; j--){
-
-                if ((9 - i + j) % 2 == 0){
-                    out.print(SET_BG_COLOR_BLACK);
+        if (whitePerspective) {
+            for (int i = 8; i > 0; i--) {
+                printSide(i, out);
+                for (int j = 1; j < 9; j++) {
+                    printBoardRow(i, j, out, theBoard);
                 }
-                else {
-                    out.print(SET_BG_COLOR_WHITE);
-                }
-
-                ChessPiece piece = theBoard.getPiece(new ChessPosition(i, j));
-
-                if (piece == null){
-                    out.print("   ");
-                }
-                else{
-                    printPiece(out, piece);
-                }
+                printSide(i, out);
+                out.print(RESET_BG_COLOR);
+                out.print("\n");
             }
-            out.print(SET_BG_COLOR_LIGHT_GREY);
-            out.print(" ");
-            out.print(SET_TEXT_COLOR_WHITE);
-            out.print(i);
-            out.print(" ");
-            out.print(RESET_BG_COLOR);
-            out.print("\n");
+        }
+        else {
+            for (int i = 1; i < 9; i++) {
+                printSide(i, out);
+                for (int j = 8; j > 0; j--) {
+                    printBoardRow(i, j, out, theBoard);
+                }
+                printSide(i, out);
+                out.print(RESET_BG_COLOR);
+                out.print("\n");
+            }
+        }
+    }
+
+    private static void printSide(int rowNumber, PrintStream out){
+        out.print(SET_BG_COLOR_LIGHT_GREY);
+        out.print(" ");
+        out.print(SET_TEXT_COLOR_WHITE);
+        out.print(rowNumber);
+        out.print(" ");
+    }
+
+    private static void printBoardRow(int row, int column, PrintStream out, ChessBoard board){
+        if ((9 - row + column) % 2 == 0) {
+            out.print(SET_BG_COLOR_WHITE);
+        } else {
+            out.print(SET_BG_COLOR_BLACK);
         }
 
+        ChessPiece piece = board.getPiece(new ChessPosition(row, column));
 
-
-//        out.print(SET_TEXT_COLOR_DARK_BLUE);
-//        out.print(SET_BG_COLOR_WHITE);
-//        out.print(" r ");
-//        out.print(SET_BG_COLOR_DARK_GREEN);
-//        out.print(" n ");
-//        out.print(SET_BG_COLOR_WHITE);
-//        out.print(" b ");
+        if (piece == null) {
+            out.print("   ");
+        } else {
+            printPiece(out, piece);
+        }
     }
 
     private static void printPiece(PrintStream out, ChessPiece piece){
         ChessGame.TeamColor color = piece.getTeamColor();
         ChessPiece.PieceType type = piece.getPieceType();
 
-        //fix piece orientation
 
         if (color == ChessGame.TeamColor.BLACK) {
             out.print(SET_TEXT_COLOR_DARK_BLUE);
@@ -146,18 +156,25 @@ public class PrintBoard {
 
     }
 
-    private static void drawTopOrBottomRow(PrintStream out){
+    private static void drawTopOrBottomRow(PrintStream out, boolean whitePerspective){
+        ArrayList<String> letters = new ArrayList<>(Arrays.asList("a","b","c","d","e","f","g","h"));
+
         out.print(SET_BG_COLOR_LIGHT_GREY);
         out.print("   ");
-        out.print(" a ");
-        out.print(" b ");
-        out.print(" c ");
-        out.print(" d ");
-        out.print(" e ");
-        out.print(" f ");
-        out.print(" g ");
-        out.print(" h ");
+
+        if (!whitePerspective){
+            Collections.reverse(letters);
+        }
+
+        for (String letter: letters){
+            out.print(" ");
+            out.print(letter);
+            out.print(" ");
+        }
+
         out.print("   ");
         out.print(RESET_BG_COLOR);
+        out.print("\n");
+
     }
 }
