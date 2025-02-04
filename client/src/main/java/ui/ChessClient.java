45e5ae9032c420c4c62267b3748b3ea7da3a79df
Princedg5678 <gilesdevyn6@gmail.com>
2025-02-04 12:45:56 -0700

    continued on Phase 5

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,6 +1,58 @@
 package ui;
 
+import java.util.Arrays;
+
 public class ChessClient {
 
-    //add constructor
+    //private final ServerFacade server;
+    private final String serverURL;
+    private final State currentState = State.SIGNEDOUT;
+
+
+    public ChessClient(String serverUrl){
+        this.serverURL = serverUrl;
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            switch (cmd) {
+                case "quit" -> ;
+                case "login" -> ;
+                case "register" -> ;
+                case "logout" -> ;
+                case "create" -> ;
+                case "list" -> ;
+                case "play" -> ;
+                case "observe" -> ;
+                default -> help();
+            }
+        } catch (Exception ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String help(){
+        if (currentState == State.SIGNEDOUT){
+            return """
+                    - register <username> <password> <email>
+                    - login <username> <password>
+                    - help
+                    - quit
+                    """;
+        }
+        return """
+                - create <gameName>
+                - list 
+                - play <gameNumber> <color>
+                - observe <gameNumber>
+                - help
+                - logout
+                """;
+    }
+
+
+
 }
--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,12 +1,16 @@
 package ui;
 
+import java.util.Arrays;
+
 import static ui.EscapeSequences.*;
 
 public class Repl {
     private final ChessClient client;
 
     public Repl(String serverURL){
-        client = new ChessClient(serverURL, this);
+        client = new ChessClient(serverURL/*, this*/);
     }
+
+
 
 }
