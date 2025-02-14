51b1cb2cc76f1c35cd7cd2f76b09e7de9a473508
Princedg5678 <gilesdevyn6@gmail.com>
2025-02-14 13:51:59 -0700

    worked on phase 5

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,9 +1,11 @@
 package ui;
 
+import chess.ChessGame;
 import model.*;
 import server.ServerFacade;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class ChessClient {
 
@@ -11,7 +13,7 @@ public class ChessClient {
     private State currentState = State.SIGNEDOUT;
     private final ServerFacade server;
     private String authToken;
-
+    private HashMap<Integer, GameID> IDMap = new HashMap<>();
 
     public ChessClient(String serverUrl){
         this.serverURL = serverUrl;
@@ -106,6 +108,7 @@ public class ChessClient {
         }
 
         server.logoutUser(authToken);
+        authToken = null;
 
         currentState = State.SIGNEDOUT;
         System.out.println("Logout Successful. See you next time!");
@@ -124,9 +127,12 @@ public class ChessClient {
         String tempName = params[0];
         GameName gameName = new GameName(tempName);
         GameID gameID = server.createGame(gameName, authToken);
-        //Figure out what to do with this
+        Integer gameNumber = IDMap.size() + 1;
 
-        return "Game successfully created";
+        IDMap.put(gameNumber, gameID);
+
+
+        return "Game successfully created. Game Number is " + gameNumber;
     }
 
     public String list() throws ResponseException{
@@ -136,28 +142,79 @@ public class ChessClient {
 
         GameList gameList = server.listGames(authToken);
         StringBuilder returnString = new StringBuilder();
-        int i = 1;
+        int gameNumber = 1;
 
         for (GameResult game: gameList.games()){
-            returnString.append(i).append(". \n");
+
+            GameID gameID = new GameID(game.gameID());
+            IDMap.put(gameNumber, gameID);
+
+            returnString.append(gameNumber).append(". \n");
             returnString.append("Game Name: ").append(game.gameName()).append("\n");
             returnString.append("White username: ").append(game.whiteUsername()).append("\n");
             returnString.append("Black username: ").append(game.blackUsername()).append("\n");
             returnString.append("\n");
-            i++;
+            gameNumber++;
         }
 
         return returnString.toString();
     }
 
     public String play(String... params) throws ResponseException{
+        if (currentState == State.SIGNEDOUT){
+            throw new ResponseException("Error: Not Logged in");
+        }
 
+        if (params.length < 2){
+            throw new ResponseException("Error: Expected <gameNumber> <color>");
+        }
 
-        return null;
+        Integer gameNumber = Integer.parseInt(params[0]);
+        if (!IDMap.containsKey(gameNumber)){
+            throw new ResponseException("Error: Game does not exist");
+        }
+        String playerColor = params[1];
+        if (!playerColor.equalsIgnoreCase("WHITE") && !playerColor.equalsIgnoreCase("BLACK")){
+            throw new ResponseException("Error: Invalid Color");
+        }
+
+        GameID gameID = IDMap.get(gameNumber);
+        JoinRequest joinRequest = new JoinRequest(playerColor, gameID.gameID());
+
+        server.playGame(joinRequest, authToken);
+        if (playerColor.equalsIgnoreCase("WHITE")){
+            PrintBoard.printWhitePerspective(new ChessGame());
+        }
+        else {
+            PrintBoard.printBlackPerspective(new ChessGame());
+        }
+
+        //the new ChessGame is a placeholder
+
+        return "Game Joined. Have Fun!";
     }
 
     public String observe(String... params) throws ResponseException{
+        if (currentState == State.SIGNEDOUT){
+            throw new ResponseException("Error: Not Logged in");
+        }
 
+        if (params.length < 1){
+            throw new ResponseException("Error: Expected <gameNumber>");
+        }
+
+        Integer gameNumber = Integer.parseInt(params[0]);
+        GameID gameID = IDMap.get(gameNumber);
+        GameList gameList = server.listGames(authToken);
+
+        //GameResult does not actually contain a ChessGame. Find a way to fix that. New ChessGame is placeholder
+
+        for (GameResult game: gameList.games()){
+            GameID tempID = new GameID(game.gameID());
+            if (gameID == tempID){
+              PrintBoard.printWhitePerspective(new ChessGame());
+            }
+        }
 
         return null;
     }
