c7b98a874228249c6dba1b49059d44ea3d5d3596
Princedg5678 <gilesdevyn6@gmail.com>
2025-02-25 12:22:11 -0700

    Worked on phase 6

---
+++ client/src/main/java/websocket/WebsocketFacade.java
@@ -0,0 +1,17 @@
+package websocket;
+
+import com.google.gson.Gson;
+import ui.ResponseException;
+import websocket.messages.ServerMessage;
+import websocket.commands.UserGameCommand;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class WebsocketFacade {
+
+
+
+}
